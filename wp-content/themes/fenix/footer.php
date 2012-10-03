3d8c8307278eb4ac1df6df8e91056ff110f22cec
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-10-03 10:26:29 -0500

    mismo menu en el footer

--- wp-content/themes/fenix/footer.php
+++ wp-content/themes/fenix/footer.php
@@ -1,13 +1,15 @@
 			<div class="clearfloat"></div>
 			<div id="footer">
 				<ul>
-					<li><a href="index.html">INICIO</a></li>
-					<li><a href="la_compania.html">LA COMPAÑÍA</a></li>
-					<li><a href="servicios_financieros.html">SERVICIOS FINANCIEROS</a></li>
-					<li><a href="servicios_bursatiles.html">SERVICIOS BURSÁTILES</a></li>
-					<li><a href="informes.html">INFORMES</a></li>
-					<li class="last-footer-link"><a href="contacto.html">CONTACTO</a></li>
-					<li class="social-icon-footer"> <a href="#" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/icon_twitter.png" width="35" height="35" alt="twitter" title="síguenos en twitter"></a></li>
+					<?php
+					wp_nav_menu( array(
+						'theme_location' => 'primary',
+						'container' => false,
+						'items_wrap' => '%3$s',
+						'depth' => 1,
+					) );
+					?>
+					<li class="social-icon-footer first-social-icon"> <a href="#" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/icon_twitter.png" width="35" height="35" alt="twitter" title="síguenos en twitter"></a></li>
 					<li class="social-icon-footer"> <a href="#" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/icon_facebook.png" alt="facebook" title="visítanos en facebook" width="35" height="35"></a></li>
 					<li class="email-footer-link"><a href="#">EMAIL CORPORATIVO</a></li>
 				</ul>
